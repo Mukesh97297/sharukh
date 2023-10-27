3ad43745e8f8a6408bd54ef6918e972749db613f
Mukesh <mkverma97297@gmail.com>
2023-10-28 03:51:17 +0530

    responsiveness

--- src/app/page.js
+++ src/app/page.js
@@ -2,15 +2,20 @@ import Navbar from './components/Navbar'
 import Footer from './components/Footer'
 import Home1 from './components/Home';
 import Services from './components/Services';
+import Contact from './components/Contact';
+import SlickCarousel from './components/SlickCarousel';
+
 
 export default function Home() {
   return (
     <>
-       <Navbar/>
+       <Navbar/> 
        <Home1/>
        <Services/>
-       <Footer/>
-     
+       <SlickCarousel/>
+       <Contact/>
+       <Footer/> 
+      
       
    </>
   )
