3f05691d77ad4edf9ec47b92b3f616b2830a1599
Mukesh <mkverma97297@gmail.com>
2023-10-28 17:54:21 +0530

    new buld

--- src/app/components/SlickCarousel.jsx
+++ src/app/components/SlickCarousel.jsx
@@ -3,8 +3,7 @@
 import React, { useState } from 'react';
 import Slider from 'react-slick';
 import Image from 'next/image';
-import 'slick-carousel/slick/slick.css';
-import 'slick-carousel/slick/slick-theme.css';
+
 
 const SlickCarousel = () => {
     const [currentSlide, setCurrentSlide] = useState();
--- src/app/layout.js
+++ src/app/layout.js
@@ -1,6 +1,8 @@
 import './globals.css'
 import { Inter } from 'next/font/google'
-
+import Head from 'next/head'
+import 'slick-carousel/slick/slick.css';
+import 'slick-carousel/slick/slick-theme.css';
 const inter = Inter({ subsets: ['latin'] })
 
 export const metadata = {
@@ -11,7 +13,9 @@ export const metadata = {
 export default function RootLayout({ children }) {
   return (
     <html lang="en">
-      <meta name="viewport" content="width=device-width, initial-scale=1"></meta>
+     <Head>
+     <meta name="viewport" content="width=device-width, initial-scale=1"></meta>
+     </Head>
       
       <body className={inter.className}>
         
